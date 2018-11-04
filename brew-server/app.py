ffa43e7a69e48abc63b9e2350bd64d616206d283
Matthias Welsh <welsh_matthias@bah.com>
2018-11-03 23:43:40 -0400

    format string

--- brew-server/app.py
+++ brew-server/app.py
@@ -27,11 +27,11 @@ def read_temp():
         temp_string = lines[1][equals_pos+2:]
         temp_c = float(temp_string) / 1000.0
         temp_f = temp_c * 9.0 / 5.0 + 32.0
-        return temp_c, temp_f
+        return temp_f
 
 @app.route('/')
 def index():
-  return read_temp()
+  return  "Current temp %f" % read_temp()
 
 if __name__ == '__main__':
   app.debug = debug
