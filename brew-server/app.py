8b10bae2049fe583029b1567474d765061560e9b
Matthias Welsh <welsh_matthias@bah.com>
2018-11-03 23:40:32 -0400

    Reorganize

--- brew-server/app.py
+++ brew-server/app.py
@@ -11,17 +11,6 @@ base_dir = '/sys/bus/w1/devices/'
 device_folder = glob.glob(base_dir + '28*')[0]
 device_file = device_folder + '/w1_slave'
 
-@app.route('/')
-def index():
-  return read_temp()
-
-if __name__ == '__main__':
-  app.debug = debug
-  app.run(host='0.0.0.0')
-
-os.system('modprobe w1-gpio')
-os.system('modprobe w1-therm')
-
 def read_temp_raw():
     f = open(device_file, 'r')
     lines = f.readlines()
@@ -39,3 +28,14 @@ def read_temp():
         temp_c = float(temp_string) / 1000.0
         temp_f = temp_c * 9.0 / 5.0 + 32.0
         return temp_c, temp_f
+
+@app.route('/')
+def index():
+  return read_temp()
+
+if __name__ == '__main__':
+  app.debug = debug
+  app.run(host='0.0.0.0')
+
+os.system('modprobe w1-gpio')
+os.system('modprobe w1-therm')
