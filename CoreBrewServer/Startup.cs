9a6dd2a3a06b6d8c24538e262afa1341792c6519
mawelsh <welsh_matthias@bah.com>
2019-07-27 21:17:04 -0400

    start by just using http

--- CoreBrewServer/Startup.cs
+++ CoreBrewServer/Startup.cs
@@ -60,7 +60,7 @@ namespace CoreBrewServer
             else
             {
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
+                //app.UseHsts();
             }
 
             app.UseCors(builder => builder
@@ -74,7 +74,7 @@ namespace CoreBrewServer
                 c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
             });
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
             app.UseMvc();
         }
 
